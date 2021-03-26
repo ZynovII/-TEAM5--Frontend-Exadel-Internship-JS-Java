4169f7b2729631d63f4a5485489e9666d36a79d3
lol123a10-debug <ayubkhon.gulomov@gmail.com>
2021-03-26 18:28:01 +0500

    test

---
+++ src/components/Pages/events.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Events extends Component {
+  render() {
+    return (
+      <section className='events'>
+        <div className="wrapper">
+          <div className="events__title-wrapper">
+            <h1 className='events__title'>Internship JS & Java</h1>
+            <p className='events__subtitle'>01.03.2021 - 15.04.2021, Belarus</p>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+
+export default Events;
