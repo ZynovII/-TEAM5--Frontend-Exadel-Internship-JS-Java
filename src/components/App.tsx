ea13fc460f950a2ce8a4541e7849f4c1bd57e486
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2021-03-30 12:48:48 +0300

    feature: add header (#4)
    Co-authored-by: Igor Zynov <62754121+ZynovII@users.noreply.github.com>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,7 @@
 import { hot } from "react-hot-loader/root";
 import * as React from "react";
+import 'office-ui-fabric-core/dist/css/fabric.css'; 
+
 
 import "./../assets/scss/App.scss";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
