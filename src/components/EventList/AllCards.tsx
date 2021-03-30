5d94e0eda064125e75edf22bd89006e4b1c0d7cd
lena-r-a <75614947+lena-r-a@users.noreply.github.com>
2021-03-30 12:07:21 +0300

    Implement initial screen to allow user select courses (#7)

---
+++ src/components/EventList/AllCards.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {CardItem, ICardItemInfo} from './EventCard'
+import './AllCards.scss'
+
+const events = [{
+    id: 1,
+    title: 'Java&JavaScript',
+    date: '01.03.2021-31.05.2021',
+    location: 'Belarus'
+},
+{
+    id: 2,
+    title: 'Java&JavaScript',
+    date: '01.03.2021-31.05.2021',
+    location: 'Belarus'
+},
+{
+    id: 3,
+    title: 'Java&JavaScript',
+    date: '01.03.2021-31.05.2021',
+    location: 'Belarus'
+}
+]
+
+export const AllCards : React.FC = ()=> {
+    return (
+        <section className="all-cards__wrapper">
+            {events.map((obj: ICardItemInfo)=><CardItem key={obj.id} cardItem={obj}/>)}
+            
+         </section>
+
+    );
+}
+
+
