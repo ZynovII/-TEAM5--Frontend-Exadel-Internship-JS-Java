08ab2cfee3a7166bfd0f2273e207f22d9178f55e
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2021-03-31 12:06:19 +0300

    feature: add modal window for login button (#9)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
-
+import  {  initializeIcons  }  from  '@fluentui/react/lib/Icons' ; 
 const rootEl = document.getElementById("root");
-
+initializeIcons();
 render(<App />, rootEl);
