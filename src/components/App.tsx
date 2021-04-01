ea56bad2744f6914bb0578339c715e4fa7eac0bd
zynov <zynov1997@mail.ru>
2021-04-01 18:59:59 +0300

    implement NotFound page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,24 @@
 import { hot } from "react-hot-loader/root";
 import * as React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import 'office-ui-fabric-core/dist/css/fabric.css'; 
+import "office-ui-fabric-core/dist/css/fabric.css";
 
 import "./../assets/scss/App.scss";
 import { MainPage } from "../pages/MainPage";
 import { Store } from "../context/Store";
+import { NotFoundPage } from "../pages/NotFoundPage";
 
 const App = () => {
-    return (
-      <Store>
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={MainPage} />
-            </Switch>
-        </BrowserRouter>
-      </Store>
-    );
+  return (
+    <Store>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </BrowserRouter>
+    </Store>
+  );
 };
 
 export default hot(App);
