a6cf93164b7a021b835794828591b51b31956faf
zynov <zynov1997@mail.ru>
2021-04-03 05:38:08 +0300

    identations fix

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export const NotFoundPage = () => (
-    <div>
-        <span className="ms-fontSize-68">404</span>
-        <br />
-        <span>... Page not found</span>
-    </div>
+	<div>
+		<span className="ms-fontSize-68">404</span>
+		<br />
+		<span>... Page not found</span>
+	</div>
 );
