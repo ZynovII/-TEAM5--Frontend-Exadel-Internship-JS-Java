ebafadb1164e78d81ab2a5c5cb38ee184fe8bf02
zynov <zynov1997@mail.ru>
2021-04-05 18:47:33 +0300

    Resize page without content, indent style editor*

--- src/components/DescrioptionEventPage/DescriptionEvenPage.tsx
+++ src/components/DescrioptionEventPage/DescriptionEvenPage.tsx
@@ -1,54 +1,65 @@
-import React from 'react'
-import { getTheme } from '@fluentui/react'
+import React from "react";
+import { getTheme } from "@fluentui/react";
 
 const theme = getTheme();
 const styles = {
-    img: {
-        paddingTop: 5,
-        width: 520,
-        maxWidth: '100%',
-        height: 'auto',
-        display: 'block',
-        paddingBottom:5 
-    },
-    div: {
-        margin: '0 auto 140px'
-    }
-}
-export interface ICardItemInfo{
-    id: number,
-    title: string;
-    date: string;
-    location: string,
-    description: string,
+  img: {
+    paddingTop: 5,
+    width: 520,
+    maxWidth: "100%",
+    height: "auto",
+    display: "block",
+    paddingBottom: 5,
+  },
+  div: {
+    margin: "0 auto 140px",
+  },
+};
+export interface ICardItemInfo {
+  id: number;
+  title: string;
+  date: string;
+  location: string;
+  description: string;
 }
 export interface ICardItemProps {
-    cardItem: ICardItemInfo;
+  cardItem: ICardItemInfo;
 }
 const eventImg = require("./../../assets/img/event_img.jpg");
-export const DescriptionEventPage: React.FC<ICardItemProps> = (props) =>{
-    return (
-        <div className="ms-Grid d-flex" style={{ boxShadow: theme.effects.elevation64, width: '73%', margin:'0 auto' }}>
-            <div className="ms-Grid-row">
-                <div className="ms-Grid-col ms-sm4 ms-md6 ms-lg6">
-                    <img style={styles.img} src={eventImg.default} alt="event"/>
-                </div>
-                <div className="ms-Grid-col ms-sm8 ms-md6 ms-lg6">
-                    {/* <p>{props.cardItem.description}</p> */}
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis tempore quo perspiciatis 
-                        quia hic. Maiores libero fugiat officia in ab reiciendis, animi, blanditiis eligendi dolorum eius,
-                        ipsam architecto dolor quo!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis tempore quo perspiciatis 
-                        quia hic. Maiores libero fugiat officia in ab reiciendis, animi, blanditiis eligendi dolorum eius,
-                        ipsam architecto dolor quo!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis tempore quo perspiciatis 
-                        quia hic. Maiores libero fugiat officia in ab reiciendis, animi, blanditiis eligendi dolorum eius,
-                        ipsam architecto dolor quo!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis tempore quo perspiciatis 
-                        quia hic. Maiores libero fugiat officia in ab reiciendis, animi, blanditiis eligendi dolorum eius,
-                        ipsam architecto dolor quo!
-                    
-                    </p>
-                </div>
-            </div>
+export const DescriptionEventPage: React.FC<ICardItemProps> = (props) => {
+  return (
+    <div
+      className="ms-Grid d-flex"
+      style={{
+        boxShadow: theme.effects.elevation64,
+        width: "73%",
+        margin: "0 auto",
+      }}
+    >
+      <div className="ms-Grid-row">
+        <div className="ms-Grid-col ms-sm4 ms-md6 ms-lg6">
+          <img style={styles.img} src={eventImg.default} alt="event" />
         </div>
-    )
-}
+        <div className="ms-Grid-col ms-sm8 ms-md6 ms-lg6">
+          {/* <p>{props.cardItem.description}</p> */}
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
+            tempore quo perspiciatis quia hic. Maiores libero fugiat officia in
+            ab reiciendis, animi, blanditiis eligendi dolorum eius, ipsam
+            architecto dolor quo!Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Veritatis tempore quo perspiciatis quia hic.
+            Maiores libero fugiat officia in ab reiciendis, animi, blanditiis
+            eligendi dolorum eius, ipsam architecto dolor quo!Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Veritatis tempore quo
+            perspiciatis quia hic. Maiores libero fugiat officia in ab
+            reiciendis, animi, blanditiis eligendi dolorum eius, ipsam
+            architecto dolor quo!Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Veritatis tempore quo perspiciatis quia hic.
+            Maiores libero fugiat officia in ab reiciendis, animi, blanditiis
+            eligendi dolorum eius, ipsam architecto dolor quo!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 import FilterAll from "./Filter/FilterAll";
 import { AllCards } from "./EventList/AllCards";
 
 const MainComponent: React.FC = () => {
-  return (
-    <main>
-        <h1 style={{textAlign:'center'}}>You can start your career <br />with Exadel</h1>
-        <h2 className="title">Filter</h2>
-        <FilterAll />
-        <h2 className="title">All Events</h2>
-        <AllCards />
-    </main>
-  )
-}
+	return (
+		<main className="main">
+			<h1 style={{ textAlign: "center" }}>
+				You can start your career <br />
+				with Exadel
+			</h1>
+			<h2 className="title">Filter</h2>
+			<FilterAll />
+			<h2 className="title">All Events</h2>
+			<AllCards />
+		</main>
+	);
+};
 
-export default MainComponent
+export default MainComponent;
