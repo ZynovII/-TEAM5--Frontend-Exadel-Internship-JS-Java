bd1b860f2251d71e5e8bc745adad1e89b7540877
zynov <zynov1997@mail.ru>
2021-04-05 22:32:26 +0300

    wire up fakeDB to AllCards

--- src/components/HOCs/withRequstHOC.tsx
+++ src/components/HOCs/withRequstHOC.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Spinner, SpinnerSize } from "@fluentui/react";
 
 export const withRequest = (WrapedComponent, request) => {
-  return async ({ ...props }) => {
-    const data = await request();
-    return <WrapedComponent {...props} data={data} />;
+  return ({ ...props }) => {
+    const [data, setData] = useState();
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+      request.then((res) => {
+        setData(JSON.parse(res));
+        setLoading(false);
+      });
+    }, []);
+    return isLoading ? (
+      <Spinner size={SpinnerSize.large} />
+    ) : (
+      <WrapedComponent {...props} data={data} />
+    );
   };
 };
--- src/fakeDB/fakeRequest.ts
+++ src/fakeDB/fakeRequest.ts
@@ -1,6 +1,10 @@
-import * as applicants from "./applicants.json";
-import * as events from "./events.json";
-import * as users from "./users.json";
+import * as applicantsData from "./applicants.json";
+import * as eventsData from "./events.json";
+import * as usersData from "./users.json";
+
+const applicants = JSON.stringify(Object.assign([], applicantsData));
+const events = JSON.stringify(Object.assign([], eventsData));
+const users = JSON.stringify(Object.assign([], usersData));
 
 export const fakeRequest = (data) => {
   return new Promise((resolve, reject) => {
