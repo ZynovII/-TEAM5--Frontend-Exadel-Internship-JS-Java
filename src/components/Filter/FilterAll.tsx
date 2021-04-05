02f45b0389381e16dd159f6cdc9343df1fcfa1dc
AnastasiyaKiulo <81416475+AnastasiyaKiulo@users.noreply.github.com>
2021-04-05 04:39:57 -0500

    Filter update (#20)
    * Filter update
    
    * Filter Update
    
    Co-authored-by: Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>

--- src/components/Filter/FilterAll.tsx
+++ src/components/Filter/FilterAll.tsx
@@ -4,36 +4,44 @@ import { IFilterItem, DropdownControlledExample } from "./FilterItem";
 const filters = [
   {
     id: 1,
-    placeholder: "All",
-    label: "Event type",
-    options: ["Meet-up", "Training", "Internship"],
-  },
-  {
+    placeholder: 'All',
+    label: 'Event type',
+    options: [{key: 'Meet-up', text: 'Meet-up'}, {key: 'Training', text: 'Training'}, {key: 'Internship', text: 'Internship'}],
+    
+},
+{
     id: 2,
-    placeholder: "All",
-    label: "Speciality",
-    options: ["HR", "DevOps"],
-  },
-  {
+    placeholder: 'All',
+    label: 'Speciality',
+    options: [{key: 'HR', text: 'HR'}, {key: 'DevOps', text: 'DevOps'}],
+    
+},
+{
     id: 3,
-    placeholder: "All",
-    label: "Locations",
-    options: ["Belarus", "Poland", "Ucraine", "Russia"],
-  },
-];
+    placeholder: 'All',
+    label: 'Locations',
+    options: [{key: 'USA', text: 'USA'}, {key: 'Belarus', text: 'Belarus'}, {key: 'Russia', text: 'Russia'}, {key: 'Ukraine', text: 'Ukraine'}],
+    
+}
+]
 
 const filterDisplay = {
-  display: "flex",
-  paddingLeft: "121px",
+    display: 'flex',
+    paddingLeft: '121px',
+    paddingRight: '121px'
+
+} as const;
+const filterPadding = {
+    paddingRight: '121px',
+
 } as const;
 
-export const AllFilters: React.FC = () => {
-  return (
-    <section style={filterDisplay}>
-      {filters.map((obj: IFilterItem) => (
-        <DropdownControlledExample filterItem={obj} />
-      ))}
-    </section>
-  );
-};
-export default AllFilters;
+export const AllFilters : React.FC = ()=> {
+    return (
+        <section style={filterDisplay}>
+            {filters.map((obj: IFilterItem)=><div style={filterPadding}><DropdownControlledExample key={obj.id } filterItem={obj}/></div>)}
+         </section>
+        
+    );
+}
+export default AllFilters
--- src/components/Filter/FilterItem.tsx
+++ src/components/Filter/FilterItem.tsx
@@ -5,57 +5,37 @@ import {
   IDropdownStyles,
 } from "@fluentui/react/lib";
 
-const dropdownStyles: Partial<IDropdownStyles> = {
-  dropdown: { width: 300, paddingRight: "121px", borderRadius: "18px" },
-};
 
-export interface IFilterItem {
-  id: number;
-  placeholder: string;
-  label: string;
-  options: string[];
+const dropdownStyles: Partial<IDropdownStyles> = { dropdown: { width: 300,} };
+
+export interface IFilterItem{
+    id: number;
+    placeholder: string;
+    label: string;
+    options: object[];
 }
 
 export interface IFilterItemProps {
   filterItem: IFilterItem;
 }
 
-export const DropdownControlledExample: React.FC<IFilterItemProps> = (
-  props
-) => {
-  const [selectedKeys, setSelectedKeys] = React.useState<string[]>([]);
+export const DropdownControlledExample: React.FC<IFilterItemProps> = (props) => {
+  const [selectedItem, setSelectedItem] = React.useState<IDropdownOption>();
 
-  const onChange = (
-    event: React.FormEvent<HTMLDivElement>,
-    item: IDropdownOption
-  ): void => {
-    if (item) {
-      setSelectedKeys(
-        item.selected
-          ? [...selectedKeys, item.key as string]
-          : selectedKeys.filter((key) => key !== item.key)
-      );
-    }
+  const onChange = (event: React.FormEvent<HTMLDivElement>, item: IDropdownOption): void => {
+    setSelectedItem(item);
   };
 
-  return (
-    <Dropdown
-      label={props.filterItem.label}
-      selectedKeys={selectedKeys}
-      // eslint-disable-next-line react/jsx-no-bind
-      onChange={onChange}
-      placeholder={props.filterItem.placeholder}
-      options={props.filterItem.options.valueOf()}
-      styles={dropdownStyles}
-    />
-  );
-};
-
-export const FilterItem: React.FC<IFilterItemProps> = (props) => (
-  <Dropdown
-    placeholder={props.filterItem.placeholder}
-    label={props.filterItem.label}
-    option={props.filterItem.options}
-  />
-  </Dropdown>
-);
+  
+    return (
+      <Dropdown
+        label={props.filterItem.label}
+        selectedKey={selectedItem ? selectedItem.key : undefined}
+        // eslint-disable-next-line react/jsx-no-bind
+        onChange={onChange}
+        placeholder={props.filterItem.placeholder}
+        options={props.filterItem.options}
+        styles={dropdownStyles}
+      />
+    );
+  };
