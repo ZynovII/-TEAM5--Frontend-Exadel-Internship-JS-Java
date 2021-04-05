1eaea35cd14a54a32c92dfecb88d826824712415
Pavel <pavel@ymc.by>
2021-04-05 13:45:28 +0300

    feature: add filter component

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import AllFilters from "./Filter/FilterAll";
+import FilterAll from "./Filter/FilterAll";
 import { AllCards } from "./EventList/AllCards";
 
 const MainComponent: React.FC = () => {
@@ -7,7 +7,7 @@ const MainComponent: React.FC = () => {
     <main>
         <h1 style={{textAlign:'center'}}>You can start your career <br />with Exadel</h1>
         <h2 className="title">Filter</h2>
-        {/* <AllFilters /> */}
+        <FilterAll />
         <h2 className="title">All Events</h2>
         <AllCards />
     </main>
