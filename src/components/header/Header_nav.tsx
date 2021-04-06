762901de4832ce7c65747449b72b1242e3d1fa8f
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2021-04-06 12:40:56 +0300

    Header navigation (add links) (#24)
    * feature: combine components
    
    * feature: add filter component
    
    * feature: add navigation for header navigation

--- src/components/header/Header_nav.tsx
+++ src/components/header/Header_nav.tsx
@@ -1,28 +1,31 @@
 import { FC } from "react";
+import { useHistory } from "react-router";
 
 import { CommandBar, ICommandBarItemProps } from "@fluentui/react/lib";
 
-const _items: ICommandBarItemProps[] = [
-  {
-    key: "tasks",
-    text: "Tasks",
-    style: { fontSize: "18px" },
-  },
-  {
-    key: "events",
-    text: "Events",
-    href: "#",
-    style: { fontSize: "18px" },
-  },
-  {
-    key: "interviewes",
-    text: "Interviewes",
-    href: "#",
-    style: { fontSize: "18px" },
-  },
-];
-
 const Navigation: FC = () => {
+  const history = useHistory();
+  const _items: ICommandBarItemProps[] = [
+    {
+      key: "candidates",
+      text: "Candidates",
+      style: { fontSize: "18px" },
+      onClick: () => history.push(`admin/candidates`),
+    },
+    {
+      key: "events",
+      text: "Events",
+      onClick: () => history.push(`/admin/events`),
+      style: { fontSize: "18px" },
+    },
+    {
+      key: "interviewes",
+      text: "Interviewes",
+      onClick: () => history.push(`/admin/interviews`),
+      style: { fontSize: "18px" },
+    },
+  ];
+  
   return <CommandBar items={_items} className="ms-hiddenMdDown" />;
 };
 
