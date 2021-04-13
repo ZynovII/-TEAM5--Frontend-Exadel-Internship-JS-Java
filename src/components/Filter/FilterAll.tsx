578105679ad6093dde7b1d16f0c9645b89e3140a
Vladimir Bondarchuk <vladimir.bondarchuk109@gmail.com>
2021-04-14 00:38:55 +0300

    rebuild filter component

--- src/components/Filter/FilterAll.tsx
+++ src/components/Filter/FilterAll.tsx
@@ -1,15 +1,18 @@
-import React,{useMemo} from "react";
-import { DropdownControlledExample } from "./FilterItem";
-import {Stack, PrimaryButton,IStackStyles } from "@fluentui/react";
-import {IFilterDropdownItem,IFilterData} from "./Models"
+import React, { useMemo } from "react";
 import {
+  Stack,
+  PrimaryButton,
+  IStackStyles,
+  IDropdownStyles,
   ITag,
+  Label,
 } from "@fluentui/react";
-import { useBoolean } from "@fluentui/react-hooks";
-import {TagPickerItem} from "./TagPicker"
+import { IFilterDropdownItem, IFilterData } from "./Models";
 import { useForm } from "react-hook-form";
-
-
+import {
+  ControlledDropdown,
+  ControlledTagPicker,
+} from "../../hook-form/ControlledTextField";
 
 const stackStyles: IStackStyles = {
   root: {
@@ -18,23 +21,50 @@ const stackStyles: IStackStyles = {
   },
 };
 
-
+const dropdownStyles: Partial<IDropdownStyles> = {
+  dropdown: {},
+  root: {
+    width: "30%",
+    minWidth: "200px",
+    margin: "0 2px",
+  },
+};
 
 export const AllFilters: React.FC = () => {
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<IFilterData>({
+    reValidateMode: "onSubmit",
+    mode: "all",
+  });
 
-  const {handleSubmit,control} = useForm<IFilterData>()
-
-  const onApplyFilter = (data) => {
-    handleSubmit((data) =>{
+  const onApplyFilter = () => {
+    handleSubmit((data) => {
       console.log(data);
-      
-    })()
-    
-  }
-  const filters:IFilterDropdownItem[] = useMemo(() => {
+      let tags = null;
+      if (Array.isArray(data.tagPicker)) {
+        tags = data.tagPicker.map((item) => {
+          return item["key"];
+        });
+      }
+
+      const dataSubmit = {
+        ...data,
+        location: data.location["key"],
+        eventType: data.eventType["key"],
+        tagPicker: tags,
+      };
+
+      console.log(dataSubmit);
+    })();
+  };
+  const filters: IFilterDropdownItem[] = useMemo(() => {
     return [
       {
         id: 1,
+        name: "eventType",
         placeholder: "All",
         label: "Event type",
         options: [
@@ -45,6 +75,7 @@ export const AllFilters: React.FC = () => {
       },
       {
         id: 3,
+        name: "location",
         placeholder: "All",
         label: "Locations",
         options: [
@@ -55,37 +86,86 @@ export const AllFilters: React.FC = () => {
         ],
       },
     ];
-  },[])
+  }, []);
 
-  const eventTags: ITag[] =  useMemo(() => {
+  const eventTags: ITag[] = useMemo(() => {
     return [
-      'JavaScript',
-      'Java',
-      'Python',
-      'React',
-      'Web',
-      'Frontend',
-      'Backend',
+      "JavaScript",
+      "Java",
+      "Python",
+      "React",
+      "Web",
+      "Frontend",
+      "Backend",
       "c#",
       "Junior",
       "TypeScript",
       "Data base",
-    
-    ].map(item => ({ key: item.toLowerCase(), name: item }));
-  }, [])
+    ].map((item) => ({ key: item.toLowerCase(), name: item }));
+  }, []);
+
+  const listContainsTagList = (tag: ITag, tagList?: ITag[]) => {
+    if (!tagList || !tagList.length || tagList.length === 0) {
+      return false;
+    }
+    return tagList.some((compareTag) => compareTag.key === tag.key);
+  };
+
+  const filterSuggestedTags = (filterText: string, tagList: ITag[]): ITag[] => {
+    return filterText
+      ? eventTags.filter(
+          (tag) =>
+            tag.key.toString().indexOf(filterText.toLowerCase()) === 0 &&
+            !listContainsTagList(tag, tagList)
+        )
+      : [];
+  };
 
   return (
     <>
-      <Stack styles={stackStyles} horizontal verticalAlign="center" horizontalAlign="space-between" wrap>
+      <Stack
+        styles={stackStyles}
+        horizontal
+        verticalAlign="center"
+        horizontalAlign="space-between"
+        wrap
+      >
         {filters.map((obj: IFilterDropdownItem) => (
-          <div key={obj.id}>
-            <DropdownControlledExample filterItem={obj} />
-          </div>
+          <ControlledDropdown
+            label={obj.label}
+            key={obj.id}
+            control={control}
+            name={obj.name}
+            placeholder={obj.placeholder}
+            options={obj.options}
+            errors={errors}
+            styles={dropdownStyles}
+          />
         ))}
-        <TagPickerItem label = "Tags" eventTags={eventTags}></TagPickerItem>
+
+        <Stack.Item
+          align="center"
+          styles={{
+            root: { margin: "0 2px", minWidth: "200px", width: "30%" },
+          }}
+        >
+          <Label>Tags</Label>
+          <ControlledTagPicker
+            name="tagPicker"
+            control={control}
+            eventTags={eventTags}
+            onResolveSuggestions={filterSuggestedTags}
+            itemLimit={eventTags.length}
+            aria-label="Tag picker"
+          />
+        </Stack.Item>
       </Stack>
       <div className="margin2em button_center">
-        <PrimaryButton onClick = {onApplyFilter} text="Search" className="button" />
+        <PrimaryButton
+          onClick={onApplyFilter}
+          text="Search"
+          className="button"
+        />
       </div>
     </>
   );
--- src/hook-form/HookFormProps.ts
+++ src/hook-form/HookFormProps.ts
@@ -1,9 +1,12 @@
 import { Control, FieldErrors, UseControllerProps } from "react-hook-form";
+import { ITag } from "@fluentui/react";
 
 export interface HookFormProps {
   control: Control<any>;
   name: string;
-  errors: FieldErrors<any>;
+  errors?: FieldErrors<any>;
   rules?: UseControllerProps["rules"];
   defaultValue?: any;
+  label?: string;
+  eventTags?: ITag[];
 }
