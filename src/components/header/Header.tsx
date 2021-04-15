7408797063d7cf4dc4f9c65edc15bf2205460785
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2021-04-15 12:42:49 +0300

    fix: add log out mechanics to header (#60)
    * fix: add log out mechanics to header
    
    * fix: refactoring header component
    
    * Update AuthBtn.tsx
    
    * Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,29 +1,27 @@
-import { FC, useState } from "react";
-
+import React from "react";
+import { useStore } from "../../hooks/hooks"
 import { Stack } from "@fluentui/react/lib";
+import { ActionTypes } from "../../context/actionTypes"
 
 import HeaderNav from "./Header_nav";
 import ButtonLog from "./Button_log";
 
-import "./Header.scss";
-
 const logo = require("./../../assets/img/logo.svg");
 
-const Header: FC = () => {
-	const [status, setStatus] = useState({
-		isLoggedIn: true
-	})
+const Header: React.FC = () => {
+	const {state, dispatch} = useStore()
 	const logOut = () => {
-		setStatus({isLoggedIn: false})
+		dispatch( {type: ActionTypes.SIGN_OUT} )
 	}
+
 	return (
-		<header className="header">
+		<header style={{padding: '2em'}}>
 			<Stack horizontal horizontalAlign="space-between">
 				<div style={{display:"flex", alignItems:'center', width:"550px"}}>
           <img src={logo.default} height="24" width="150" alt="exadel" />
-          <HeaderNav />
+          {state.isAuthenticated && <HeaderNav />}
         </div>
-				<ButtonLog isLoggedIn={status.isLoggedIn} userName = {'Admin'} logout={logOut} />
+				<ButtonLog isLoggedIn={ state.isAuthenticated } userName={ 'Admin' } logout={ logOut } /> //dinamic userName
 			</Stack>
 		</header>
 	);
