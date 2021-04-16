2fa76070c00de8d45cddd9b0e951c7518c22f486
Ayubkhon Gulomov <ayubkhon.gulomov@gmail.com>
2021-04-16 14:23:37 +0500

    Scrollbar (#63)
    * New Feature: Scrollbar;
    
    * .
    
    * .

---
+++ src/components/UI/Scrollbar/Scrollbar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { mergeStyleSets } from '@fluentui/react'
+import { Scrollbars } from "react-custom-scrollbars";
+
+const styles = mergeStyleSets({
+  Scrollbar: {
+    backgroundColor: 'rgba(0,0,0, .2)',
+    zIndex: 9999
+  }
+})
+
+const Scrollbar = props => (
+  <Scrollbars
+    style={{ width: "100%", height: "100vh" }}
+    autoHide
+    renderThumbVertical={(props) => (
+      <div
+        {...props}
+        className={styles.Scrollbar}
+      />
+    )}
+  >
+    {props.children}
+  </Scrollbars>
+)
+
+export default Scrollbar;
