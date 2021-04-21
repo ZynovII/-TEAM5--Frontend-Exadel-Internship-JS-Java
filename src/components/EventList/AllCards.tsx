b9358d7eee24e24e3e675e673282d7680de4196a
Ayubkhon Gulomov <ayubkhon.gulomov@gmail.com>
2021-04-21 14:37:25 +0500

    Added. (#76)

--- src/components/EventList/AllCards.tsx
+++ src/components/EventList/AllCards.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
 import { CardItem } from "./EventCard";
-import { NewCardItem } from "../NewEvent/NewCardItem"
+import { NewCardItem } from "../NewEvent/NewCardItem";
 import { PrimaryButton, Spinner, SpinnerSize } from "@fluentui/react";
 import "./AllCards.scss";
 import { useEvents, useStore } from "../../hooks/hooks";
+import Wrapper from "../UI/Wrapper/Wrapper";
 
 export const AllCards: React.FC = () => {
   const { events, loading, fechEvents } = useEvents();
@@ -15,19 +16,21 @@ export const AllCards: React.FC = () => {
     <Spinner size={SpinnerSize.large} className="margin2em" />
   ) : (
     <>
-      <section className="all-cards__wrapper">
-        {state.isAuthenticated && <NewCardItem />}
-        {Object.keys(events).map((id) => (
-          <CardItem
-            cardItem={events[id]}
-            key={id}
-            isLogged={state.isAuthenticated}
-          /> // useAuth()
-        ))}
-      </section>
-      <div className="margin2em button_center">
-        <PrimaryButton text="Load More" className="button" />
-      </div>
+      <Wrapper>
+        <section className="all-cards__wrapper">
+          {state.isAuthenticated && <NewCardItem />}
+          {Object.keys(events).map((id) => (
+            <CardItem
+              cardItem={events[id]}
+              key={id}
+              isLogged={state.isAuthenticated}
+            /> // useAuth()
+          ))}
+        </section>
+        <div className="margin2em button_center">
+          <PrimaryButton text="Load More" className="button" />
+        </div>
+      </Wrapper>
     </>
   );
 };
