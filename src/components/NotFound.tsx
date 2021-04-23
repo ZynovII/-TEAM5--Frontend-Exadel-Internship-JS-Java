fc12746bc55563478c8aebe446de281523648ebf
Pavel <pavel@ymc.by>
2021-04-24 01:24:02 +0300

    fix: changed text for 404 error

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -4,22 +4,25 @@ import React from "react";
 
 const titleStyle = {
   fontSize: "68px",
-  paddingLeft: "2rem",
   color: getTheme().palette.themePrimary,
 };
+const spanClassTitle = mergeStyles({
+  padding: "1rem 1rem 0.5rem 1rem",
+  fontSize: "25px",
+  // color: getTheme().palette.themeTertiary,
+});
 const spanClass = mergeStyles({
-  padding: "1rem",
   fontSize: "20px",
-  color: getTheme().palette.themeTertiary,
 });
 const className = mergeStyles(spanClass, {
   padding: "2rem",
+  textAlign: 'center',
 });
 
 export const NotFound = () => (
   <div className={className}>
-    <span style={titleStyle}>404</span>
-    <br />
-    <span className={spanClass}>Page not found...</span>
+    <p style={titleStyle}>404</p>
+    <p className={spanClassTitle}>We are sorry, the page you requested cannot be found.</p>
+    <p className={spanClass}>The URL may be misspelled or the page you're looking for is no longer available.</p>
   </div>
 );
