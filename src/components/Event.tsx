5418d73803211d116e388d99ad6d65f26ec719bb
zynov <zynov1997@mail.ru>
2021-04-24 16:43:46 +0300

    refactoring

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -4,23 +4,19 @@ import { IEvent } from "../models/IEvent";
 import { DescriptionEventPage } from "./DescrioptionEventPage/DescriptionEvenPage";
 import { Registration } from "./Registration/Registration";
 
-import Wrapper from "./UI/Wrapper/Wrapper";
-
 export interface IEventProps {
   data: IEvent;
 }
 
 const Event: React.FC<IEventProps> = (props) => {
   return (
-    <main className="main">
-      <Wrapper>
-        <h1 style={{ textAlign: "center", marginBottom: "1em" }}>
-          Internship JS & Java
-        </h1>
-        <DescriptionEventPage cardItem={props.data} />
-        <Registration name="Js && Java" />
-      </Wrapper>
-    </main>
+    <>
+      <h1 style={{ textAlign: "center", marginBottom: "1em" }}>
+        Internship JS & Java
+      </h1>
+      <DescriptionEventPage cardItem={props.data} />
+      <Registration name="Js && Java" />
+    </>
   );
 };
 
