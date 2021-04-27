43ffdb4aafc35693f1eee7dffbe4ab1527a5d890
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2021-04-27 12:20:01 +0300

    Update InterwievForm.tsx

--- src/components/CandidatePage/InterwievForm.tsx
+++ src/components/CandidatePage/InterwievForm.tsx
@@ -18,58 +18,6 @@ const Interviewer: { key: string; text: string }[] = [
   { key: "Yan", text: "Yan" },
 ];
 
-const dayPickerStrings = {
-  months: [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ],
-  shortMonths: [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ],
-  days: [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ],
-  shortDays: ["S", "M", "T", "W", "T", "F", "S"],
-  goToToday: "Go to today",
-  weekNumberFormatString: "Week number {0}",
-  prevMonthAriaLabel: "Previous month",
-  nextMonthAriaLabel: "Next month",
-  prevYearAriaLabel: "Previous year",
-  nextYearAriaLabel: "Next year",
-  prevYearRangeAriaLabel: "Previous year range",
-  nextYearRangeAriaLabel: "Next year range",
-  closeButtonAriaLabel: "Close",
-  monthPickerHeaderAriaLabel: "{0}, select to change the year",
-  yearPickerHeaderAriaLabel: "{0}, select to change the month",
-};
-
 const time = [
   {
     key: 1,
@@ -149,7 +97,6 @@ export const InterviewForm: React.FC = () => {
             name={"dateInterview"}
             label="Choose date"
             showMonthPickerAsOverlay={true}
-            strings={dayPickerStrings}
             placeholder="Select a date..."
             ariaLabel="Select a date"
           />
