6680a765b82def6b7fbe7cc984fdbf75a2a7a8cd
Igor Zynov <62754121+ZynovII@users.noreply.github.com>
2021-04-30 21:01:17 +0300

    Update express.js

--- express.js
+++ express.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const request = require("request");
 const app = express();
 const portNumber = 3000;
 const sourceDir = "dist";
@@ -11,20 +10,3 @@ app.listen(portNumber, () => {
   console.log(`Serving content from /${sourceDir}/`);
 });
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-
-app.post("/api/candidates", (req, res) => {
-  request(
-    { url: "https://localhost:8081/api/candidates" },
-    (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: "error", message: err.message });
-      }
-
-      res.json(JSON.parse(body));
-    }
-  );
-});
