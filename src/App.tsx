7c503349b727ede8db05f81816ce6c7cf6c273e2
Vladimir.Bondarchuk <78910749+git941@users.noreply.github.com>
2021-04-30 13:26:19 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import Scrollbar from "./components/UI/Scrollbar/Scrollbar";
 import TSPage from "./components/TSPage/TSPage";
 const App = () => (
   <Store>
-    <BrowserRouter>
+    <HashRouter>
       <Scrollbar>
         <Switch>
           <Route path="/" exact>
@@ -26,11 +26,11 @@ const App = () => (
           </Route>
           <Route path="/events" component={MainPage} />
           <Route path="/admin" component={AdminPage} />
-          <Route path="/techinterview" component={TSPage} />
+          <Route path="/techinterview:id" component={TSPage} />
           <Route component={NotFoundPage} />
         </Switch>
       </Scrollbar>
-    </BrowserRouter>
+    </HashRouter>
   </Store>
 );
 
