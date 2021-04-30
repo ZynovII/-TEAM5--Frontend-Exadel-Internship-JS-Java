0a7b687bab775f4774a0aa96fa103a02b8e88b0e
Vladimir Bondarchuk <vladimir.bondarchuk109@gmail.com>
2021-04-30 03:29:17 +0300

    routed tspage

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,23 @@ import { AdminPage } from "./pages/AdminPage";
 import { Store } from "./context/Store";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import Scrollbar from "./components/UI/Scrollbar/Scrollbar";
+import TSPage from "./components/TSPage/TSPage";
 
 const App = () => (
   <Store>
-    <HashRouter>
+    <BrowserRouter>
       <Scrollbar>
         <Switch>
           <Route path="/" exact>
             <Redirect to="/events" />
           </Route>
+          <Route path="/techinterview:id" component={TSPage} />
           <Route path="/events" component={MainPage} />
           <Route path="/admin" component={AdminPage} />
           <Route component={NotFoundPage} />
         </Switch>
       </Scrollbar>
-    </HashRouter>
+    </BrowserRouter>
   </Store>
 );
 
