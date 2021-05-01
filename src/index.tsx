4d243f2bbbbbe3cb55ae02c48b43e0c7c369d3a2
ayubkhon.gulomov@gmail.com <ayubkhon.gulomov@gmail.com>
2021-05-01 16:01:14 +0500

    added loadTheme;

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,14 @@ import * as React from "react";
 import { render } from "react-dom";
 import App from "./App";
 import { initializeIcons } from "@fluentui/react/lib/Icons";
+import { loadTheme, createTheme } from "@fluentui/react";
+loadTheme(
+  createTheme({
+    defaultFontStyle: {
+      fontFamily: "Roboto",
+    },
+  })
+);
 const rootEl = document.getElementById("root");
 initializeIcons();
 render(<App />, rootEl);
