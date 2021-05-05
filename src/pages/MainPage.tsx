b83e203f2ef6ae4f0cbe435a3a841abf2c2a268e
ayubkhon.gulomov@gmail.com <ayubkhon.gulomov@gmail.com>
2021-05-05 14:15:00 +0500

    main-page grid footer at bottom;

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -10,22 +10,23 @@ import MainComponent from "../components/MainComponent";
 import Event from "../components/Event";
 import Scrollbar from "../components/UI/Scrollbar/Scrollbar";
 
-
 export const MainPage = () => {
   return (
     <>
       <Scrollbar height="100%">
-        <Header />
-        <main className="main">
-          <Wrapper>
-            <Switch>
-              <Route path="/events" exact component={MainComponent} />
-              <Route path="/events/:id" component={Event} />
-              <Route component={NotFound} />
-            </Switch>
-          </Wrapper>
-        </main>
-        <Footer />
+        <div className="main-page__wrapper">
+          <Header />
+          <main className="main">
+            <Wrapper>
+              <Switch>
+                <Route path="/events" exact component={MainComponent} />
+                <Route path="/events/:id" component={Event} />
+                <Route component={NotFound} />
+              </Switch>
+            </Wrapper>
+          </main>
+          <Footer />
+        </div>
       </Scrollbar>
     </>
   );
