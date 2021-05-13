26619c2b64382db6095bf1de62e4893dc56d6d24
Ayubkhon Gulomov <ayubkhon.gulomov@gmail.com>
2021-05-13 15:40:14 +0500

    Axios instance (#131)
    * added;
    
    * added ability to get token from localStorage;
    
    * hotfix
    
    * hotfix
    
    * reconfing
    
    * added;

--- src/axios-instance.tsx
+++ src/axios-instance.tsx
@@ -4,15 +4,13 @@ const instance = axios.create({
   baseURL: "http://localhost:8081/api",
 });
 
-const auth = () => {
+(() => {
   if (localStorage.getItem("token")) {
     return (instance.defaults.headers.common[
       "Authorization"
     ] = localStorage.getItem("token"));
   }
   return;
-};
-
-auth();
+})();
 
 export default instance;
