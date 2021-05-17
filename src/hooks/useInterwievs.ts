4dc9eeab8810dce1d2a87d34b2881ab6606e15b9
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2021-05-17 12:33:01 +0300

    wire up employees dropdowns (#135)
    * wire up employees dropdowns
    
    * Update InterwievForm.tsx

--- src/hooks/useInterwievs.ts
+++ src/hooks/useInterwievs.ts
@@ -1,6 +1,6 @@
 import { ActionTypes } from "../context/actionTypes";
 import { useStore } from "./hooks";
-
+import axios from "../axios-api";
 export const useInterviews = () => {
   const { state, dispatch } = useStore();
   const fetchInterviews = () => {
@@ -15,10 +15,24 @@ export const useInterviews = () => {
     dispatch({ type: ActionTypes.SELECT_INTERVIEW, id });
   };
 
+  const getRoles = async () => {
+      const response = await axios.get('/employees/roles')
+      return response.data
+  };
+
+  const getInterviewers = async () => {
+    axios.get('/employees/interviewers/list/').then(res => {
+      dispatch({ type: ActionTypes.FETCH_INTERVIEWERS, payload: res.data});
+    })
+  }
+
   return {
     selectedInterview: state.selectedInterview,
     interviews: state.applicants,
+    interviewers: state.interviewers,
     selectInterview,
     fetchInterviews,
+    getRoles,
+    getInterviewers,
   };
 };
