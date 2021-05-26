1268bd94c01d98f399835b074f9337b6b14e116e
Igor Zynov <62754121+ZynovII@users.noreply.github.com>
2021-05-26 12:30:30 +0300

    refactoring events list with filters (#148)

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import FilterAll from "./Filter/FilterAll";
 import EventList from "./EventList/EventList";
 
 const MainComponent: React.FC = () => {
@@ -9,10 +8,7 @@ const MainComponent: React.FC = () => {
         You can start your career <br />
         with Exadel
       </h1>
-      <h2 className="title">Filter</h2>
-      <FilterAll />
-      <h2 className="title">All Events</h2>
-      <EventList isAdminPage={false}/>
+      <EventList isAdminPage={false} />
     </>
   );
 };
