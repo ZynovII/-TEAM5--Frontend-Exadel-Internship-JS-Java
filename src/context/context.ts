97aed856ac15f66fa68847e5efad70bf57cfe60f
zynov <zynov1997@mail.ru>
2021-05-27 19:15:38 +0300

    unfix)

--- src/context/context.ts
+++ src/context/context.ts
@@ -3,10 +3,7 @@ import { IAction } from "../models/Store/IAction";
 import { IStore } from "../models/Store/IStore";
 import { tokenToUser } from "../utils/tokenToUser";
 
-let token = null;
-if (localStorage.getItem("token")) {
-  token = JSON.parse(localStorage.getItem("token"));
-}
+let token = localStorage.getItem("token") || null;
 
 export const initialState: IStore = {
   isAuthenticated: !!token || false,
